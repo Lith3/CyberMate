304d232f9dc6c4ea7cee028dd8b45701e9397cf1
JorisB <jorisbudin@gmail.com>
2024-07-17 09:18:01 +0200

    reset harmonia and add CSS root in App.css

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,25 @@
 create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
-  password varchar(255) not null
+  password varchar(255) not null,
+  role varchar(20) default "user"
 );
 
-create table item (
+create table topic (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null,
   user_id int unsigned not null,
+  date DATE NOT NULL,
+  subject VARCHAR(255) not null,
   foreign key(user_id) references user(id)
+);
+
+create table message (
+  id int unsigned primary key auto_increment not null,
+  user_id int unsigned not null,
+  date DATE NOT NULL,
+  content text not null,
+  topic_id int unsigned not null,
+  foreign key (user_id) references user(id),
+    FOREIGN KEY (topic_id) references topic(id)
 );
