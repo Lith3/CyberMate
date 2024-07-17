f220e7083be2354ba14a3c33ba34fdabe163de7e
JorisB <jorisbudin@gmail.com>
2024-07-17 16:37:51 +0200

    The navBar changes if the user is logged in

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,16 +1,35 @@
 import { NavLink } from "react-router-dom";
-import styles from "./NavBar.module.css";
+import "./NavBar.css";
+import { useContext } from "react";
+import { AuthentificationContext } from "../../assets/use_context/Authentification";
 
 function NavBar() {
+  const { auth } = useContext(AuthentificationContext);
+
   return (
     <nav>
-      <span>TeST</span>
-      <NavLink className={styles.navItem} to="/profil">
-        Profil
+      <NavLink className="navItem" to="/topics">
+        Topics
       </NavLink>
-      <NavLink className={styles.navItem} to="/connexion">
-        Déconnexion
-      </NavLink>
+      {auth === true ? (
+        <>
+          <NavLink className="navItem" to="/profil">
+            Profil
+          </NavLink>
+          <NavLink className="navItem" to="/connexion">
+            Déconnexion
+          </NavLink>{" "}
+        </>
+      ) : (
+        <>
+          <NavLink className="navItem" to="/inscription">
+            Inscription
+          </NavLink>
+          <NavLink className="navItem" to="/connexion">
+            Connexion
+          </NavLink>{" "}
+        </>
+      )}
     </nav>
   );
 }
